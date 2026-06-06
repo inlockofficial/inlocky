a9b170673587c5efd825b651c6a3ba5b3ce9f2a3
KADARI hafsa <275335024+inlockofficial@users.noreply.github.com>
2026-06-06 19:25:27 +0100

    Update 2026_04_04_210913_remove_user_id_from_products_table.php

--- database/migrations/2026_04_04_210913_remove_user_id_from_products_table.php
+++ database/migrations/2026_04_04_210913_remove_user_id_from_products_table.php
@@ -12,6 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        
+        Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('user_id');
         });
     }
