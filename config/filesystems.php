2e74bba4dfc6e4d86c466ba1f779f9be69cc90fe
KADARI hafsa <275335024+inlockofficial@users.noreply.github.com>
2026-06-10 22:50:19 +0100

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,6 +59,15 @@ return [
             'throw' => false,
             'report' => false,
         ],
+          'cloudinary' => [
+          'driver' => 'cloudinary',
+          'key' => env('CLOUDINARY_KEY'),
+          'secret' => env('CLOUDINARY_SECRET'),
+          'cloud' => env('CLOUDINARY_CLOUD_NAME'),
+          'url' => env('CLOUDINARY_URL'),
+          'secure' => (bool) env('CLOUDINARY_SECURE', true),
+          'prefix' => env('CLOUDINARY_PREFIX'),
+        ],
 
     ],
 
@@ -76,14 +85,5 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
     ],
-      'cloudinary' => [
-          'driver' => 'cloudinary',
-          'key' => env('CLOUDINARY_KEY'),
-          'secret' => env('CLOUDINARY_SECRET'),
-          'cloud' => env('CLOUDINARY_CLOUD_NAME'),
-          'url' => env('CLOUDINARY_URL'),
-          'secure' => (bool) env('CLOUDINARY_SECURE', true),
-          'prefix' => env('CLOUDINARY_PREFIX'),
-      ],
     
 ];
