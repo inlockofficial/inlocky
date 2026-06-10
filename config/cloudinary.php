330b1ca3438397b783335f5f03c43980605effb4
KADARI hafsa <275335024+inlockofficial@users.noreply.github.com>
2026-06-10 21:32:57 +0100

    Update cloudinary.php

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -9,4 +9,6 @@ return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key' => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'notification_url' => env('CLOUDINARY_NOTIFICATION_URL'),
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 ];
