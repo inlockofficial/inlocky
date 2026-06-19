0696a06720a1511e99de18838843b4f372805f0c
KADARI hafsa <275335024+inlockofficial@users.noreply.github.com>
2026-06-20 00:00:03 +0100

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,25 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = [
-        'user_id',
-        'product_id',
-        'product_title',
-        'product_image',
-        'price_usd',
-        'rate_used',
-        'total_dzd',
-        'status',
-        'expires_at',
+    'user_id',
+    'product_id',
+    'product_title',
+    'product_image',
+    'selected_size',   
+    'selected_color',  
+    'custom_note',     
+    'quantity',        
+    'price_usd',
+    'rate_used',
+    'total_dzd',
+    'status',
+    'expires_at',
 
-        // checkout info
-        'full_name',
-        'email',
-        'phone',
-        'address',
-        'city',
-        'postal_code',
-        'notes',
-    ];
+    // checkout info
+    'full_name',
+    'email',
+    'phone',
+    'address',
+    'city',
+    'postal_code',
+    'notes', // (This is for shipping notes, which is separate from the product custom_note)
+];
 
     protected $casts = [
         'expires_at' => 'datetime',
